3e3664cc4fa0a4d0b36cbf52de2e70608807b64d
CharliePayne1 <charliepayne1101@gmail.com>
2020-04-25 06:50:15 +0100

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,37 @@
+require "pry"
+
 class Student
+    attr_accessor :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, status, instructor)
+        BoatingTest.new(self, test_name, status, instructor)
+    end
+
+    def self.find_student(first_name)
+        Student.all.select {|student| student.first_name == first_name}
+    end
+
+    def total_tests
+        BoatingTest.all.select {|boatingTest| boatingTest.student == self}.length.to_f
+    end
+
+    def passed_tests
+        BoatingTest.all.select {|boatingTest| boatingTest.student == self && boatingTest.status == "passed"}.length.to_f
+    end
+
+    def grade_percentage
+        (passed_tests / total_tests) * 100
+    end
 
 end
